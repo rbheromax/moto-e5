355f3db711f814bd8d45002e3a0725db1767a806
Czar Dixon <rbheromax@gmail.com>
2018-12-31 19:55:11 -0500

    git: add prima/pronto source code

--- submodules.sh
+++ submodules.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone https://github.com/rbheromax/AnyKernel2
 git clone https://github.com/rbheromax/gcc-linaro-7.1.1 ../gcc-linaro-7.1.1
+git clone -b oreo-8.0.0-release-rhannah https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-prima drivers/staging/prima
